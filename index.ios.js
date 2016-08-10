075687655a236ed420c9614c293ea0b22565c7df
Steve Boyle <stevemboyle@Steves-MacBook-Air.local>
2016-08-10 15:07:31 -0700

    adding 'Hello World'

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+// Tutorial found here: https://facebook.github.io/react-native/docs/tutorial.html
+
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
+
+AppRegistry.registerComponenet('HelloWorldApp', () => HelloWorldApp );
